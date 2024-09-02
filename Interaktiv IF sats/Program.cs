64d60a7f6167cd4f8187b6b55d224b53760f02e1
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-09-02 11:52:37 +0200

    if sats när ja

--- Interaktiv IF sats/Program.cs
+++ Interaktiv IF sats/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-class System.console
+﻿// bool test = true || false;
+
+// Console.WriteLine(test);
+
+System.Console.WriteLine("Du vaknar i en misstänksam lastbil och inser att du har blivit kidnappad. Vad gör du? ");
+System.Console.WriteLine("Skrika för hjälp        försöka att få tag på din mobil");
+string Panic = Console.ReadLine();
+{
+
+    if (Panic == "Skrika för hjälp")
+
+    {
+        Console.WriteLine("Du skriker för hjälp tills kidnapparen blev sur, han stannade bilen och kommer emot dig");
+    }
+
+    string Desicion = Console.ReadLine();
+    if (Desicion == "försöka att få tag på mobilen);
+    {
+        Console.WriteLine("Är du dum? Såklart tog han din mobil! ahem, jag menar du insåg att mobilen var inte i dina fickor");
+    }
+}
+
+// else 
+// {
+//     Console.WriteLine("Aja baja, det får du inte göra för att jag tycker de XD");
+// }
+
+Console.ReadLine();
