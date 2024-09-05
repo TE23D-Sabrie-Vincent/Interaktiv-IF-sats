859406977c540a62b1fc5fb49173f15f0c61a2d4
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-09-05 09:48:13 +0200

    skibidi

--- Interaktiv IF sats/Program.cs
+++ Interaktiv IF sats/Program.cs
@@ -2,24 +2,40 @@
 
 // Console.WriteLine(test);
 
+using System.Runtime.CompilerServices;
+
 System.Console.WriteLine("Du vaknar i en misstänksam lastbil och inser att du har blivit kidnappad. Vad gör du? ");
 System.Console.WriteLine("Skrika för hjälp        försöka att få tag på din mobil");
-string Panic = Console.ReadLine();
-{
+string Decision = Console.ReadLine().ToLower();
 
-    if (Panic == "Skrika för hjälp")
 
-    {
-        Console.WriteLine("Du skriker för hjälp tills kidnapparen blev sur, han stannade bilen och kommer emot dig");
-    }
+    if (Decision == "skrika för hjälp")
+{ 
+Console.WriteLine("Du skriker för hjälp tills kidnapparen blev sur, han stannade bilen och kommer emot dig");
 
-    string Desicion = Console.ReadLine();
-    if (Desicion == "försöka att få tag på mobilen);
-    {
-        Console.WriteLine("Är du dum? Såklart tog han din mobil! ahem, jag menar du insåg att mobilen var inte i dina fickor");
-    }
+System.Console.WriteLine("Vad gör du nu?");
+System.Console.WriteLine("Skrika för ditt liv och försöka att sparka hen        Spela död");
+     Decision = Console.ReadLine().ToLower();
+
+
 }
+     if (Decision == "Försök att rymma")
 
+
+
+
+
+
+// {
+//     Panic = Console.ReadLine();
+//     else if (Panic == "försök att rymma")
+//     {
+//         Console.WriteLine("Är du dum? Såklart tog han din mobil! ahem, jag menar du insåg att mobilen var inte i dina fickor");
+//     }
+
+
+// }
+ 
 // else 
 // {
 //     Console.WriteLine("Aja baja, det får du inte göra för att jag tycker de XD");
